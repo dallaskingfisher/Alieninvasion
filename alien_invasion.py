2ee4244de9c80dd7ab5158e3fe3ab69e34f842e0
dallaskingfisher <dallaskingfisher@icloud.com>
2024-08-16 11:30:08 -0500

    first commit

---
+++ alien_invasion.py
@@ -0,0 +1,58 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior """
+    def __init__(self):
+        """Initialize the game, and create game resource"""
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            self._check_events()
+            self.ship.update()
+            # Redraw the screen during each pass through the loop.
+            self._update_screen()
+
+    def _check_events(self):
+        """Response to keypress and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+
+    def _update_screen(self):
+        """Update the screen images"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    # Make new game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
